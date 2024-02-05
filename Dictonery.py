97382bf5cfa5a9042632f3a7d074c4fefd1fe42e
unknown <fajardillah25@gmail.com>
2024-02-05 16:43:38 +0700

    updated

---
+++ Dictonery.py
@@ -0,0 +1,13 @@
+data_list = ["ayam", "kambing", "russa"]
+print(data_list)
+
+data_dict = {
+    "key":"value",
+    "fjr":"fajar",
+    "snd":"sandy",
+    "ryn":"ryan",
+    "hewan":data_list #kita bisa memasukan data apa saja untuk valuenya
+}
+
+print(data_dict["snd"])## kita bisa mendapatkan value dengan menulikan index pada dengan keynya
+print(data_dict["hewan"])
